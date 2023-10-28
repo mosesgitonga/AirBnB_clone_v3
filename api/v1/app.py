1f0969e757689c08379b253c9558e47d3e80e847
mosesgitonga <infosec947@gmailc.om>
2023-10-28 12:22:03 -0700

    task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,12 +5,13 @@ register blueprint
 
 from models import storage
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, make_response, jsonify
 
 app = Flask(__name__)
 #make json printing beautiful
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def teardown_db(err):
@@ -18,6 +19,15 @@ def teardown_db(err):
     teardown
     """
     storage.close()
+
+
+@app.errorhandler(404)
+def handle_404(error):
+    """
+    handle 404 errors(page not found)
+    """
+    return make_response(jsonify({"error": "Not found"}), 404)
+
 
 if __name__ == '__main__':
     import os 
