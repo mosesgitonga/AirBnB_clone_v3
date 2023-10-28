f04fad6b7dde17c0edf9b9cad5d6f90ab02a4517
mosesgitonga <infosec947@gmailc.om>
2023-10-27 23:46:13 -0700

    merge with storage_get_count

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,8 @@ from api.v1.views import app_views
 from flask import Flask
 
 app = Flask(__name__)
+#make json printing beautiful
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
@@ -14,4 +16,4 @@ if __name__ == '__main__':
     import os 
     host = os.getenv('HBNB_API_HOST', default='0.0.0.0')
     port = os.getenv('HBNB_API_PORT', default=5000)
-    app.run(host, int(port), threaded=True)
+    app.run(host=host, port=int(port), threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,12 +5,10 @@ create a route /status on the object app_views that returns a JSON
 from api.v1.views import app_views
 from flask import Flask, jsonify
 
-app = Flask(__name__)
-
-@app.route('/status', strict_slashes=False)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def  ok_status():
     status = {
-                "status": "ok"
+                "status": "OK"
               }
     return jsonify(status)
     
