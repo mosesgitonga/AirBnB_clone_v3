e4780bc740c15c975d5326e3aa209d00e8e35a93
Mariangithubit <metsyb1@hotmail.com>
2023-10-29 16:02:09 -0700

    update base model

--- models/base_model.py
+++ models/base_model.py
@@ -58,8 +58,8 @@ class BaseModel:
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self):
-        """returns a dictionary containing all keys/values of the instance"""
+    def to_dict(self, save_fs=None):
+        """returns a dictionary containing all keys/values"""
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
             new_dict["created_at"] = new_dict["created_at"].strftime(time)
@@ -68,6 +68,9 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
+        if save_fs is None:
+            if "password" in new_dict:
+                del new_dict["password"]
         return new_dict
 
     def delete(self):
